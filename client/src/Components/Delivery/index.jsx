1ccd993b2476747aa889ef28aabb2fdc6dba11ec
PrathamMalvia <malviapratham1@gmail.com>
2022-10-30 15:20:14 +0530

    add Delivery section

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Components
 import DeliveryCarousel from './DeliveryCarousel';
@@ -6,11 +6,61 @@ import Brand from './Brand';
 import RestaurantCard from '../RestaurantCard';
 
 const Delivery = () => {
+
+    const [restaurantList, setRestaurantList] = useState([
+        {
+            _id: "123456-3",
+            photos: [
+                "https://b.zmtcdn.com/data/pictures/chains/4/51154/858232a18b274a7279a47e59567c6557.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+            ],
+            name: "Samosa Party",
+            cuisine: ["North Indian", "Chinese"],
+            averageCost: 100,
+            isPro: true,
+            isOff: 80,
+            durationOfDelivery: 47,
+            restaurantReviewValue: 4.1,
+        },
+        {
+            _id: "123456-2",
+            photos: [
+                "https://b.zmtcdn.com/data/pictures/chains/4/51154/858232a18b274a7279a47e59567c6557.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+            ],
+            name: "Al Bek",
+            cuisine: ["North Indian", "Chinese"],
+            averageCost: 100,
+            isPro: true,
+            isOff: 80,
+            durationOfDelivery: 47,
+            restaurantReviewValue: 4.1,
+        },
+        {
+            _id: "123456-1",
+            photos: [
+                "https://b.zmtcdn.com/data/pictures/chains/4/51154/858232a18b274a7279a47e59567c6557.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+            ],
+            name: "New Punjabi Hotel",
+            cuisine: ["North Indian", "Chinese"],
+            averageCost: 100,
+            isPro: true,
+            isOff: 80,
+            durationOfDelivery: 47,
+            restaurantReviewValue: 4.1,
+        },
+        
+    ]);
+
     return (
         <>
             <DeliveryCarousel />
             {/* <Brand /> */}
-            <RestaurantCard />
+            <div className='flex justify-between flex-wrap'>
+                {
+                    restaurantList.map((restaurant) => (
+                        <RestaurantCard {...restaurant} key={restaurant._id} />
+                    ))
+                }
+            </div>
         </>
     )
 }
