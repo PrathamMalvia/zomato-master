1c3fb80968b14b8ccff1aaf560705de716a40c30
PrathamMalvia <malviapratham1@gmail.com>
2022-12-15 18:19:34 +0530

    add cart bar

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -9,6 +9,7 @@ import ImageGrid from '../Components/restaurant/ImageGrid';
 import RestaurantInfo from '../Components/restaurant/RestaurantInfo';
 import InfoButtons from '../Components/restaurant/InfoButtons';
 import TabContainer from '../Components/restaurant/Tabs';
+import CartContainer from '../Components/Cart/CartContainer';
 
 const RestaurantLayout = (props) => {
     return (
@@ -55,8 +56,8 @@ const RestaurantLayout = (props) => {
                     </TabContainer>
                 </div>
                 {props.children}
-
             </div>
+            <CartContainer />
 
 
 
